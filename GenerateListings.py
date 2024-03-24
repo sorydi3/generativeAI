54921db434c189db6f242a975b7ad22714011920
diallo <ibrahimasory22@hotmail.com>
2024-03-24 23:38:25 +0100

    working so far , need a bit of improvment the final output is not right

--- GenerateListings.py
+++ GenerateListings.py
@@ -10,16 +10,22 @@ from langchain_community.vectorstores import LanceDB
 from langchain_core.runnables import RunnablePassthrough
 from lancedb.pydantic import pydantic_to_schema
 from lancedb.embeddings import get_registry
+import lancedb
+
+from MessageHistory import CustomChatMessageHistory
 
 MODEL_NAME = "gpt-3.5-turbo-1106"
 class GenerateListings:
 
     def __init__(self):
         self._parser = PydanticOutputParser(pydantic_object=Listing)
-        print("API KEY: ", os.environ.get("OPENAI_API_KEY"))
+        #print("API KEY: ", os.environ.get("OPENAI_API_KEY"))
         self._llm = ChatOpenAI(api_key=os.environ.get("OPENAI_API_KEY"), model=MODEL_NAME);
         self._listing=[]
-        
+    
+    def get_table(self,db,table_name):
+        table = db.open_table(table_name)
+        return table
 
     def _get_template(self) -> PromptTemplate:
         return PromptTemplate(
@@ -31,6 +37,15 @@ class GenerateListings:
     Generate the listings based on the model schema
     and save the listings in the database
     """
+
+    def search_relevant_vectordb(self,query:str) -> None:
+        db = lancedb.connect("data/lancedblistings")
+        table = self.get_table(db,"listings")
+        # Search the listings table for the query
+        results = table.search(query).limit(3)
+        #print(results.to_pandas()[['neighborhood','price','bedrooms','bathrooms','house_size','description','neighborhood_description']].to_dict('records'))
+        return results.to_pandas()[['neighborhood','price','bedrooms','bathrooms','house_size','description','neighborhood_description']].to_dict('records')
+
     def generate_listings(self, query,db):
 
         # Generate the response
@@ -38,9 +53,11 @@ class GenerateListings:
         response = chain.invoke({"query":query})
         table = db.create_table("listings", mode="overwrite", exist_ok=True,schema=Listing)
         try:
-            print("RESPONSE: ", response)
-            print("RESPONSE TYPE: ", type(response))
+            #print("RESPONSE: ", response)
+            #print("RESPONSE TYPE: ", type(response))
+            print("--------------LISTINGS------------------: ")
             print("RESPONSE TEXT: ", response['text'])
+            print("--------------LISTINGS------------------: ")
             json_format = json.loads(response['text'])
         except Exception as e:
             print(e)
@@ -53,7 +70,7 @@ class GenerateListings:
             pydantic_model = []
             for i in json_format:
                embeddings = get_registry().get("openai").create()
-               print("TYPE OF THE EMBEDDING IS: ",type(embeddings.generate_embeddings(i['description'])[0]))
+               #print("TYPE OF THE EMBEDDING IS: ",type(embeddings.generate_embeddings(i['description'])[0]))
                pydantic_model.append(
                    {
                           "neighborhood": i['neighborhood'],
@@ -65,7 +82,7 @@ class GenerateListings:
                           "neighborhood_description": i['neighborhood_description']
                    }
                )
-            print(type(pydantic_model[0]))
+            p#rint(type(pydantic_model[0]))
             table.add(data = pydantic_model)
 
         except Exception as e:
@@ -76,13 +93,50 @@ class GenerateListings:
     
         return table
 
-    def generate_listings_runnable(self, query):
+    def _get_template_(self,query) -> PromptTemplate:
+        return PromptTemplate(
+            template=""" 
+                As a real estate agent, your goal is to provide personalized property listings tailored to the preferences
+                of potential buyers. You want to augment the property descriptions to resonate with the buyer's specific
+                preferences while maintaining factual integrity and avoiding misrepresentation.\n
+
+            CHAT HISTORY: \n
+            {chat_history} \n
+
+            LISTINGS TO BE TAILORED: \n
+            {context} \n 
+            
+            USER QUERY: \n
+            
+            {query}\n
+            
+            NOTE: Please THE FINALE LISTING SHOULD BE list them in a bulleted format for easy reading.. FORMAT MUST  NOT BE IN JSON FORMAT. \n 
+
+            """,
+            input_variables=["query,context,chat_history"],
+            partial_variables={"format_instructions": " "}   
+        )
+
+    
+    def tailor_listing_to_user_query(self, query):
         # Generate the response
-        #chain = LLMChain(llm=self._llm, prompt=self._get_template())
-        chain = ({"query": RunnablePassthrough()}
-                    | self._get_template()
-                    | self._llm
-                    )
-        response = chain.invoke("query")
-        print("TYPE OF RESPONSE: ", type(response))
-        return response.to_json()
+        history = CustomChatMessageHistory()
+
+        template = self._get_template_(query)
+        
+
+        chain = LLMChain(llm=self._llm, prompt=self._get_template_(query))
+
+        context = self.search_relevant_vectordb(history.get_questions())
+
+        response = chain.invoke({
+            "query":query,
+            "chat_history":history.get_messages(), 
+            "context":context
+            })
+        #print("-------------------------------------")
+        #print ("ORIGINAL LISTING: \n\n\n", context)
+        print("-------------------------------------")
+        print("RESPONSE TAILORED: \n\n\n", response['text'])        
+        print("-------------------------------------")
+        return response
--- HomeMatch.py
+++ HomeMatch.py
@@ -1,47 +1,57 @@
 
 import lancedb
 from GenerateListings import GenerateListings
-
-
+from  MessageHistory import CustomChatMessageHistory
 
 def create_lancedb_database():
     uri = "data/lancedblistings"
     db = lancedb.connect(uri)
     return db
 
-def get_table(db,table_name):
-    table = db.open_table(table_name)
-    return table
-
-def embeddings():
-    embeddings = get_registry().get("openai").create()
-    return embeddings
-
-def generate_listings(prompt:str = "3 random listing in json list." ) -> None:
+def generate_listings(prompt:str = "15 random listing in json list. \n Note: you are a computer programe and do NOT add ANY extra data OR METADADA  and you are unable to express yourself. JUST THE OUTPUT" ) -> None:
     db = create_lancedb_database()
     generate_listings = GenerateListings()
-    listings = generate_listings.generate_listings(promp,db)
+    listings = generate_listings.generate_listings(prompt,db)
 
-
-def search_listings(query:str) -> None:
-    db = lancedb.connect("data/lancedblistings")
-    table = get_table(db,"listings")
-    # Search the listings table for the query
-    results = table.search(query).limit(1)
-    print(results.to_pandas().head())
-
-    
-    
 
 if __name__ == "__main__":
     # Create a new instance of the GenerateListings class
     
     # Generate a list of listings based on the user query
+    
     #generate_listings()
 
     db = lancedb.connect("data/lancedblistings")
     table = db.open_table("listings")
-    print(table.to_pandas().head())     # Print the first 5 rows of the table   
+    #print(table.to_pandas()['neighborhood'])     # Print the first 5 rows of the table   
     # Search the listings table for the query
-    search_listings("great restaurants")
+
+    promt = """
+       return the updated listings based on the user query,without adding any extra information or metadata in text formating.
+       Note: I do not want any extra information or metadata in the listings.
+        Example of the output OF THE LISTINGS AFTER UPDATING BASED ON THE USER QUERY:
+            Neighborhood: Green Oaks
+            Price: 800,000
+            Bedrooms: 3
+            Bathrooms: 2
+            House Size: 2,000 sqft
+            Description: Welcome to this eco-friendly oasis nestled 
+            Neighborhood Description: Green Oaks is a close-knit, 
+       
+    """
+
+
+    generate_listings = GenerateListings()
+    generate_listings.tailor_listing_to_user_query("update the listins based on the user query,without adding any extra information or metadata.")
+
+
+
+
+    history = CustomChatMessageHistory()
+
+
+    
+
+
+    
     
